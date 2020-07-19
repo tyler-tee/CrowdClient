1da11db4482d7f9a8cfa352922bcf4a38a268bb1
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2020-07-19 09:00:45 -0400

    Updated __init__

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(currrent_direct, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='CrowdClient',
-    version='0.1.0',
+    version='0.1.1',
     packages=['CrowdClient'],
     url='https://github.com/tyler-tee/CrowdClient',
     license='GPLv3',
